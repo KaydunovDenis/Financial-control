34d43f5e187e3db4772b8d7f94422f3b124f87da
Denis Kaydunov <deniskaydunov@gmail.com>
2022-10-04 18:50:27 +0300

    initial commit

---
+++ src/main/java/com/github/kaydunovdenis/financialcontrol/FinancialControlApplication.java
@@ -0,0 +1,11 @@
+package com.github.kaydunovdenis.financialcontrol;
+
+import org.springframework.boot.SpringApplication;
+
+public class FinancialControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialControlApplication.class, args);
+    }
+
+}
